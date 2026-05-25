588fcbceda27f52bb3497cd2bac39a39323a48ec
mohana-123 <madamohana143@gmail.com>
2026-05-25 11:29:31 +0530

    Number Guessing Game added in mini project

---
+++ top 100 code/python mini projects/number guessing game.py
@@ -0,0 +1,39 @@
+# Number Guessing Game
+'''
+## Description:
+A simple Python game where the program generates a random number and the user has to guess it.
+
+## Features:
+- Random number generation
+- User input
+- Feedback (Too high / Too low)
+- Loop until correct guess
+
+## Concepts Used:
+- Python basics
+- Loops
+- Conditionals
+- Random module
+'''
+
+import random
+
+chances = 0
+random_num = random.randint(1,100)
+print(random_num)
+print("You are given 5 chances to guess the number")
+
+while chances < 5:
+    user_input = int(input("Guess the number! => "))
+    chances +=1
+    if chances >= 5:
+        print(f"you didn't guessed it. The number is {random_num}")
+        break
+    if user_input < random_num:
+        print(f"Too low, you have used {chances} chances out of 5 to guess")
+    elif user_input > random_num:
+        print(f"Too high, you have used {chances} chances out of 5 to guess")
+    else:
+        print(f"you have guess it right! The number is {random_num}")
+        print(f"You took {chances} chances out of 5 to guess. Congrats!")
+        break
