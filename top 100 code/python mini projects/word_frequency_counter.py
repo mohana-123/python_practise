4fa361ab080e55ce3371241e78c466253e40743c
mohana-123 <madamohana143@gmail.com>
2026-06-01 12:24:26 +0530

    word frequency counter

---
+++ top 100 code/python mini projects/word_frequency_counter.py
@@ -0,0 +1,13 @@
+# word freqyency
+paragraph = "Hello my name is mohana and my bro is also mohana"
+words = paragraph.lower().split()
+frequency = {}
+for word in words:
+    words_clean = word.strip('.,!?()[]{}":;')
+
+    if words_clean in frequency:
+        frequency[words_clean] += 1
+    else:
+        frequency[words_clean] = 1
+
+print(frequency)
