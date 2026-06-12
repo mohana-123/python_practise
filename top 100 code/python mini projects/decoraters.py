7016da504a1e29ba440e6b4155f0b0fd6198ca33
mohana-123 <madamohana143@gmail.com>
2026-06-12 16:57:07 +0530

    practiced more decorators

--- top 100 code/python mini projects/decoraters.py
+++ top 100 code/python mini projects/decoraters.py
@@ -1,7 +1,7 @@
 
 import time
 from datetime import datetime, timedelta
-
+'''
 def timer_dec(base_fn):
     def enhanced_fn(*args, **kwargs):
         start_time = time.time()
@@ -30,3 +30,41 @@ def make_matcha():
 
 brew_tea(tea_type='Green',steep_time= 1)
 print(make_matcha())
+'''
+
+
+# practise ===================================================================================================================
+
+'''
+def mission_timer(base_fn):
+    def enhanced_fn(*args, **kargs):
+        print("3...")
+        time.sleep(1)
+        print("2...")
+        time.sleep(1)
+        print("1...")
+        time.sleep(1)
+        start_time = time.time()
+        result = base_fn(*args, **kargs)
+        end_time = time.time()
+        print(f"Mission duration: {end_time - start_time} seconds")
+        return result
+    return enhanced_fn
+
+
+
+@mission_timer
+def launch_probe(planet):
+    print(f"Launching probe towards {planet}...")
+    time.sleep(1)
+
+
+
+@mission_timer
+def deploy_satellite(orbit_type, altitude_km):
+    print(f"Deploying satellite into {orbit_type} at {altitude_km} km ...")
+    time.sleep(2)
+
+result = launch_probe("Europa")
+print("Stored mission result:", result)
+'''
